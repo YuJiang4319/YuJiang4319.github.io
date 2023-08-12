c99bf58838c0845c43e46839fde1a435b7ea24e3
YuJiang4319 <2923979840@qq.com>
2023-08-12 14:44:33 +0800

    update

--- git_push.bat
+++ git_push.bat
@@ -1,10 +1,3 @@
-@echo off
-rem 获取当前脚本的路径
-cd /d %~dp0
-rem 自动提交
-git init 
-git add . 
-git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
-rem git commit -m "%commitMessage%" 
-git push origin master
-@echo 已经完成
+git add .
+git commit -m "update"
+git push
